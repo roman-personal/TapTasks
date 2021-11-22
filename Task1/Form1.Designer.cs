51e5dc8d0899ded17c59b81c2338e94eaeafa61d
Roman Tabunnikov <roman.tabunnikov@hotmail.com>
2021-11-22 21:50:50 +0300

    Additional implementation

--- Task1/Form1.Designer.cs
+++ Task1/Form1.Designer.cs
@@ -26,6 +26,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -57,11 +58,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(373, 107);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Run 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(495, 162);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -77,5 +89,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Task1/Form1.cs
+++ Task1/Form1.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 
 namespace TapTasks {
     public partial class Form1 : Form {
+        const string fileName = "alice_in_wonderland.txt";
         readonly Stopwatch sw = new();
 
         public Form1() {
@@ -14,11 +15,11 @@ namespace TapTasks {
 
         private async void Button1_Click(object sender, EventArgs e) {
             sw.Restart();
-            int count = await GetLinesCount("alice_in_wonderland.txt");
+            int count = await GetLinesCount(fileName);
             sw.Stop();
             label1.Text = $"Lines count: {count}, elapsed: {sw.Elapsed}";
         }
-
+        // Используется File.OpenText - плохо с производительностью
         private async Task<int> GetLinesCount(string fileName) {
             if(string.IsNullOrEmpty(fileName))
                 throw new ArgumentException("Parameter should not be null or empty!", nameof(fileName));
@@ -35,16 +36,39 @@ namespace TapTasks {
 
         private async void Button2_Click(object sender, EventArgs e) {
             sw.Restart();
-            int count = await GetLinesCount2("alice_in_wonderland.txt");
+            int count = await GetLinesCount2(fileName);
             sw.Stop();
             label1.Text = $"Lines count: {count}, elapsed: {sw.Elapsed}";
         }
+        // Открытие потока с большим буфеом и в асинхронном режиме - лучше с производительностью
+        private async Task<int> GetLinesCount2(string fileName) {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentException("Parameter should not be null or empty!", nameof(fileName));
+            using var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None, 32768, true);
+            using var reader = new StreamReader(fs);
+            int count = 0;
+            while (true) {
+                string? line = await reader.ReadLineAsync();
+                if (line == null)
+                    break;
+                count++;
+            }
+            return count;
+        }
 
-        private Task<int> GetLinesCount2(string fileName) {
+        private async void Button3_Click(object sender, EventArgs e) {
+            sw.Restart();
+            int count = await GetLinesCount3(fileName);
+            sw.Stop();
+            label1.Text = $"Lines count: {count}, elapsed: {sw.Elapsed}";
+        }
+        // Совсем хорошо с производительностью, но с синхронным чтением и в отдельном потоке
+        private Task<int> GetLinesCount3(string fileName) {
             if (string.IsNullOrEmpty(fileName))
                 throw new ArgumentException("Parameter should not be null or empty!", nameof(fileName));
             return Task.Run(() => {
-                using var reader = File.OpenText(fileName);
+                using var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None, 32768);
+                using var reader = new StreamReader(fs);
                 int count = 0;
                 while (true) {
                     string? line = reader.ReadLine();
